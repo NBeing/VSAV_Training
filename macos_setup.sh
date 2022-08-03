70f1b49281e81a107f573dcfb6b0dd52d2bd8d93
pythmere <25112497+pythmere@users.noreply.github.com>
2022-08-03 00:30:37 -0700

    Delete macos_setup.sh

--- macos_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd "`dirname $0`"
-mv ./scripts/ ./run_vsav_training_macos.sh /Applications/FightCade2.app/Contents/MacOS/emulator/fbneo
-ln -s /Applications/FightCade2.app/Contents/MacOS/emulator/fbneo/run_vsav_training_macos.sh ~/Desktop/run_vsav_training_macos.sh

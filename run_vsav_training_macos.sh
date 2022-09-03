7b1502e15fbc30e639dfc11791ee0532dbd9499f
Terry McCall <terrymccall@gmail.com>
2022-09-03 06:53:14 -0700

    Update run_vsav_training_macos.sh
    added command that will allow script to be run from within fbneo folder wherever it is located

--- run_vsav_training_macos.sh
+++ run_vsav_training_macos.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
- ../..//../Resources/wine/bin/wine64 ./fcadefbneo.exe vsavj ./savestates/vsavj_fbneo.fs ./scripts/vsav_training_master_script.lua
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+../..//../Resources/wine/bin/wine64 ./fcadefbneo.exe vsavj ./savestates/vsavj_fbneo.fs ./scripts/vsav_training_master_script.lua

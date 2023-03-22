5b303550f4b5e62199d79cf8ed7fff24ff843e6a
N-Bee <12501551+NBeing@users.noreply.github.com>
2023-03-22 19:26:52 -0400

    muzak

--- scripts/sound.lua
+++ scripts/sound.lua
@@ -1,8 +1,8 @@
 local soundModule = {
     ["registerBefore"] = function()
        if globals.options.bgm_on then
-        memory.writebyte_audio(0xF026,0xF)
-        memory.writebyte_audio(0xF027,0xF)
+        memory.writebyte_audio(0xF026,0xFF)
+        memory.writebyte_audio(0xF027,0xFF)
        else
         memory.writebyte_audio(0xF026,0x0)
         memory.writebyte_audio(0xF027,0x0)
